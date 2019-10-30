a7151173cfac4624a31c86fc187db8a58f7fdb2d
Florentin <florentin.doerre@tu-dresden.de>
2019-10-30 09:37:53 +0100

    Generalise json to geojson conversion
    now also supports point objects

--- scripts/jsonToGeoJSON.py
+++ scripts/jsonToGeoJSON.py
@@ -1,18 +1,28 @@
 import geojson
 
-# given a list osm-ways as json (in geom format)
-def osmWaysToGeoJSON(ways):
+# given a list osm-objects as json (! in geom format!)
+def osmWaysToGeoJSON(osmObject):
     features = []
-    for way in ways:
-        points = [[pos["lon"],pos["lat"]] for pos in way["geometry"]]
-        line = geojson.LineString(points)
-        if not line.errors() is None:
-            print("Error creating line: {}".format(feature.errors))
-        feature = geojson.Feature(id=way["id"], geometry=line, properties=way["tags"])
-        if not feature.errors() is None:
-            print("Error creating feature: {}".format(feature.errors))
+    for object in osmObject:
+        geometry = osmToGeoJsonGeometry(object)
+        feature = geojson.Feature(id=object["id"], geometry=geometry, properties=object["tags"])
         features.append(feature)
     result = geojson.FeatureCollection(features)
     for error in result.errors():
-        print("Error converting osm ways to geojson: {}".format(error))
+        raise ValueError("Error converting osm object to geojson: {}".format(error))
     return result 
+
+def osmToGeoJsonGeometry(object):
+    type = object["type"]
+    points = [[pos["lon"],pos["lat"]] for pos in object["geometry"]]
+    if points is None:
+        raise ValueError('osm object has no geometry key {}'.format(object))
+    if type == "way":
+            return geojson.LineString(points, validate=True)
+    elif type == "node":
+            assert(len(points) == 1)
+            return geojson.Point(points[0], validate=True)
+    else:
+        raise ValueError('osm object {} not supported yet'.format(type))
+
+    
