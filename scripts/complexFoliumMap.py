ce50af29b5549d9c762de0cbb46f3cd100ffef68
Florentin <florentin.doerre@tu-dresden.de>
2019-11-06 23:25:54 +0100

    Generalise file naming

--- scripts/complexFoliumMap.py
+++ scripts/complexFoliumMap.py
@@ -7,15 +7,18 @@ from OSMPythonTools.nominatim import Nominatim
 from folium.plugins.measure_control import MeasureControl
 from overpassHelper import OverPassHelper
 
+# postfix for f.i. file_names
+areaName = "pieschen" 
+# area to query
 pieschen = Nominatim().query('Pieschen, Dresden, Germany')
-OverPassHelper().fetch(pieschen.areaId(), "pieschen", overrideFiles=True)
+OverPassHelper().fetch(pieschen.areaId(), areaName, overrideFiles=True)
 
 pieschenCoord = pieschen.toJSON()[0]
 map = folium.Map(
     location=[pieschenCoord["lat"], pieschenCoord["lon"]], tiles='Stamen Toner', zoom_start=15)
 
 
-file = open("out/streets_pieschen.json", encoding='UTF-8')
+file = open("out/streets_{}.json".format(areaName), encoding='UTF-8')
 all_streets = json.load(file)
 streetGroups = groupBy(all_streets, ["highway"])
 
@@ -27,7 +30,7 @@ streetsMap.add_to(map)
 # colormap = cm.get_cmap(name="BrBG", lut=len(streetGroups))
 # streetColors = {key: colormap(i) for i, key in enumerate(streetGroups.keys())}
 
-file = open("out/buildings_pieschen.json", encoding='UTF-8')
+file = open("out/buildings_{}.json".format(areaName), encoding='UTF-8')
 all_buildings = json.load(file)
 buildingGroups = groupBy(all_buildings, ["building"])
 
@@ -37,7 +40,7 @@ buildingsMap = generateFeatureCollection(
 buildingsMap.add_to(map)
 
 
-file = open("out/landuse_pieschen.json", encoding='UTF-8')
+file = open("out/landuse_{}.json".format(areaName), encoding='UTF-8')
 all_landuse = json.load(file)
 landUseGroups = groupBy(all_landuse, ["landuse"])
 
@@ -47,6 +50,6 @@ landUseMap = generateFeatureCollection(
 landUseMap.add_to(map)
 folium.LayerControl().add_to(map)
 
-fileName = "out/map_pieschen.html"
+fileName = "out/map_{}.html".format(areaName)
 map.save(fileName)
 print("Map saved in {}".format(fileName))
