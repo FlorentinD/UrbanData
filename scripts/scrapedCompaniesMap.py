40f88dd3416fca65c6d79a926a7109609039c361
Florentin <florentin.doerre@tu-dresden.de>
2019-11-26 19:23:20 +0100

    Visualize yellow page companies

---
+++ scripts/scrapedCompaniesMap.py
@@ -0,0 +1,38 @@
+import json
+import folium
+from matplotlib import cm
+from geoJsonHelper import groupBy
+from foliumHelper import geoFeatureCollectionToFoliumFeatureGroup
+from OSMPythonTools.nominatim import Nominatim
+from folium.plugins.measure_control import MeasureControl
+from OsmObjectType import OsmObjectType as OsmObject
+from overpassHelper import OverPassHelper
+from OsmDataQuery import OsmDataQuery
+
+# TODO: groupby on either amenity or building? (overlapping) (layers based on multiple tags not on single tag)
+# TODO: layers Safety, Health, Leisure, Commercial (shops, craft, building manaufture, ...), Religion,
+
+# postfix for f.i. file_names
+areaName = "pieschen"
+# area to query
+pieschen = Nominatim().query('Pieschen, Dresden, Germany')
+
+pieschenCoord = pieschen.toJSON()[0]
+map = folium.Map(
+    location=[pieschenCoord["lat"], pieschenCoord["lon"]], tiles='Stamen Toner', zoom_start=15)
+
+# matplotlib colormap names
+colormaps = ["hsv", "BrBG", "coolwarm"]
+
+#yellowPages
+file = open("out/data/scraper/yellowPages_Dresden_Pieschen.json", encoding='UTF-8')
+yellowCompanies = json.load(file)
+
+objectMap = geoFeatureCollectionToFoliumFeatureGroup(yellowCompanies, "pink", "yellowPages", switchLatAndLong = False)
+objectMap.add_to(map)
+
+folium.LayerControl().add_to(map)
+
+fileName = "out/scrapedCompaniesMap_{}.html".format(areaName)
+map.save(fileName)
+print("Map saved in {}".format(fileName))
