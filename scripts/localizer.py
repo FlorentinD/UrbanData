25a9b5133f3f1e891de89b91e392ec070b5597dc
Florentin <florentin.doerre@tu-dresden.de>
2019-12-04 18:49:10 +0100

    Fix locator based on osmWays

--- scripts/localizer.py
+++ scripts/localizer.py
@@ -4,6 +4,7 @@ from OsmObjectType import OsmObjectType
 from geoJsonHelper import osmObjectsToGeoJSON
 from shapely.geometry import mapping, shape
 from collections import defaultdict
+import re
 
 class Localizer():
     locations = None
@@ -27,13 +28,17 @@ class Localizer():
 
     # TODO: init based on regions as geojson
 
-    def locateAddress(self, street, housenumber = None, postalCode):
+    def locateAddress(self, street, postalCode, housenumber = None):
+        """f.i. for geocoding companies onto buildings instead of streets"""
         # TODO: normalize inputs
         # replace "str." with "straße"
+        street = street.replace("str.", "straße").replace("Str.","Straße")
         if not housenumber:
-            # TODO: split streetAndHouseNumber
-            # probably lowercase 3A to 3a? and remove whitespaces between in housenumbers
-            "TODO"
+            match = re.match(r"[^0-9]*(\d.*)", street)
+            street, housenumber = match.group(0), match.group(1)
+            street = street.rstrip()
+            # lowercase 3A to 3a and remove whitespaces between in housenumbers
+            housenumber = housenumber.lower().strip()
         for location in self.locations:
             locationStreet = location["properties"].get("addr:street", None)
             locationHousenumber = location["properties"].get("addr:housenumber", None)
@@ -50,34 +55,38 @@ class Localizer():
             postalCode = building["properties"].get("addr:postcode", self.DEFAULT_POSTALCODE)
             street = building["properties"].get("addr:street", self.DEFAULT_STREET)
             houseNumber = building["properties"].get("addr:housenumber", self.DEFAULT_HOUSENUMBER)
-            addresses = {postalCode: {street: houseNumber}}
+            key = self.__generateStreetsKey(postalCode, street)
+            addresses = {key: houseNumber}
         else:
             if(building["properties"]["__nodeIds"]):
                 addresses = self.addressesBasedOnOsmIds(buildingGeometry, building["properties"]["__nodeIds"])
             else:
-                addresses = defaultdict(lambda: defaultdict(list))
+                addresses = defaultdict(list)
                 for location in self.locations:
                     # contains not enough for polygons having points on edges 
                     if buildingGeometry.intersects(location["geometry"]):
                         postalCode = location["properties"].get("addr:postcode", self.DEFAULT_POSTALCODE)
                         street = location["properties"].get("addr:street", self.DEFAULT_STREET)
                         houseNumber = location["properties"].get("addr:housenumber", self.DEFAULT_HOUSENUMBER)
-                        addresses[postalCode][street].append(houseNumber)
-
+                        key = self.__generateStreetsKey(postalCode, street)
+                        addresses[key].append(houseNumber)
+                # convert back to normal dic (otherwise not hashable)
         # ! can still be empty (f.i. https://www.openstreetmap.org/way/35540321 or https://www.openstreetmap.org/way/32610207) could only be solved by taking nearest element with address 
         building["properties"]["addresses"] = addresses
         return building
 
     def addressesBasedOnOsmIds(self, buildingGeometry, nodeIds):
         matchingLocations = [loc for loc in self.locations if loc["properties"]["__nodeId"] in nodeIds]
-        addresses = defaultdict(lambda: defaultdict(list))
+        addresses = defaultdict(list)
         for location in matchingLocations:
-            # contains not enough for polygons having points on edges 
-            if buildingGeometry.intersects(location["geometry"]):
-                postalCode = location["properties"].get("addr:postcode", self.DEFAULT_POSTALCODE)
-                street = location["properties"].get("addr:street", self.DEFAULT_STREET)
-                houseNumber = location["properties"].get("addr:housenumber", self.DEFAULT_HOUSENUMBER)
-                addresses[postalCode][street].append(houseNumber)
+            postalCode = location["properties"].get("addr:postcode", self.DEFAULT_POSTALCODE)
+            street = location["properties"].get("addr:street", self.DEFAULT_STREET)
+            houseNumber = location["properties"].get("addr:housenumber", self.DEFAULT_HOUSENUMBER)
+            key = self.__generateStreetsKey(postalCode, street)
+            addresses[key].append(houseNumber)
         return addresses
+    
+    def __generateStreetsKey(self, postalCode, street):
+        return "{}, {}".format(postalCode, street)
 
-print(Localizer("Pieschen, Dresden").locateOnWeirdAddress("Kötzschenbroder Straße 193A", "01139"))
+#print(Localizer("Pieschen, Dresden").locateAddress(street="Kötzschenbroder Str. 193A", postalCode="01139"))
