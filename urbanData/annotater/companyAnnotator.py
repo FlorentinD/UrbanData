61575705b50bbab3de4e3166d149aa1ad3dd60cf
Florentin Dörre <florentin.doerre@tu-dresden.de>
2019-12-13 18:18:24 +0100

    Extract various housenumbers

--- urbanData/annotater/companyAnnotator.py
+++ urbanData/annotater/companyAnnotator.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import re
 from annotater.annotator import Annotator
 
 class CompanyAnnotator(Annotator):
@@ -14,7 +15,25 @@ class CompanyAnnotator(Annotator):
 
     @staticmethod
     def extractHousenumber(input):
-        return NotImplementedError
+        normal = r"^\d+\w?$"
+        range = r"^(\d+\w?)-(\d+\w?)$"
+        twoNumbers = r"^(\d+\w?)[\\,\/,](\d+\w?)$"
+        
+        # lowercase 3A to 3a and remove whitespaces between in housenumbers 
+        sanitizedInput = input.lower().replace(" ", "")
+        normalMatch = re.match(normal, sanitizedInput)
+        if normalMatch:
+            return sanitizedInput 
+        
+        rangeMatch = re.match(range, sanitizedInput)
+        if rangeMatch:
+            return HouseNumberRange(rangeMatch.group(1), rangeMatch.group(2))
+        else:
+            twoNumbersMatch = re.match(twoNumbers, sanitizedInput)
+            if twoNumbersMatch:
+                return [twoNumbersMatch.group(1), twoNumbersMatch.group(2)]
+        return None
+        
 
 
 @dataclass
--- urbanData/tests/test_companyAnnotator.py
+++ urbanData/tests/test_companyAnnotator.py
@@ -11,7 +11,7 @@ class TestCompanyAnnotator(unittest.TestCase):
     def test_HouseNumberExtraction(self):
         test_cases = {" 8a ": "8a", "8A": "8a", "8/10": ["8", "10"],
                         "8a/ 9f": ["8a", "9f"], "8 - 10": HouseNumberRange("8", "10"),
-                        "a": None, "8a - 10f": HouseNumberRange("8a", "10f")}
+                        "a": None, "8a - 10f": HouseNumberRange("8a", "10f"), "1a  1": None}
 
         for input, expectedResult in test_cases.items():
             result = CompanyAnnotator.extractHousenumber(input)
