595dc2a4bd34476ef905f6b812339f09760de3f4
Florentin <florentin.doerre@tu-dresden.de>
2020-01-29 22:29:06 +0100

    Convert osm polygons with holes
    also fallback to simpler shapes if this fails

--- urbanData/helper/geoJsonConverter.py
+++ urbanData/helper/geoJsonConverter.py
@@ -1,5 +1,9 @@
 import geojson
+import logging
 from shapely.geometry import mapping
+
+POLYGON_TAGS = ["building", "landuse", "area"]
+LINESTRING_TAGS = ["boundary"]
 
 def osmObjectsToGeoJSON(osmObjects):
     """given a list osm-objects as json (! in geom out-format!)"""
@@ -7,16 +11,11 @@ def osmObjectsToGeoJSON(osmObjects):
     for object in osmObjects:
         type = object["type"]
         properties = object["tags"]
-        if type == "relation":
-            relMembers = object["members"]
-            memberGeometries = [osmToGeoJsonGeometry(m) for m in relMembers]
-            geometry = geojson.MultiLineString(coordinates=memberGeometries)
-        else:
-            geometry = osmToGeoJsonGeometry(object)
-            if type == "way":
-                properties["__nodeIds"] = object["nodes"]
-            elif type == "node":
-                properties["__nodeId"] = object["id"]
+        geometry = osmToGeoJsonGeometry(object)
+        if type == "way":
+            properties["__nodeIds"] = object["nodes"]
+        elif type == "node":
+            properties["__nodeId"] = object["id"]
         feature = geojson.Feature(
             id=object["id"], geometry=geometry, properties=properties)
         features.append(feature)
@@ -28,7 +27,17 @@ def osmObjectsToGeoJSON(osmObjects):
 
 
 def osmToGeoJsonGeometry(object):
-    if "geometry" in object:
+    if object["type"] == "relation":
+            # Todo tag based
+            # TODO: use type:multipolygon
+            relMembers = object["members"]
+            outerGeometries = [osmToGeoJsonGeometry(m) for m in relMembers if m['role'] in ["outer",'']]
+            innerGeometries = [osmToGeoJsonGeometry(m) for m in relMembers if m['role'] == "inner"]    
+            coordinates = outerGeometries + innerGeometries
+            # TODO: Order members (as they can be unsorted) probably inverted index usabel for order? 
+            # (or graph algo ... longest path probably / try finding a euler-zug)
+            return tryToConvertToPolygon(object.get("tags",{}), coordinates)
+    elif "geometry" in object:
         points = [[pos["lon"], pos["lat"]] for pos in object["geometry"]]
     elif "lon" in object and "lat" in object:
         points = [[object["lon"], object["lat"]]]
@@ -37,19 +46,32 @@ def osmToGeoJsonGeometry(object):
     if points is None:
         raise ValueError('osm object has no geometry key {}'.format(object))
     if len(points) > 1:
-        polygonKeys = ["building", "landuse", "area"]
-        if(set(object.get("tags",{}).keys()).intersection(polygonKeys)):
-            polygon = geojson.Polygon([points])
-            if polygon.errors():
-                print("Could not convert to a polygon {}".format(object))
-                return geojson.LineString(points, validate=True)
-            else:
-                return polygon
-        else:
-            return geojson.LineString(points, validate=True)
+        return tryToConvertToPolygon(object.get("tags",{}), points)
     else:
         assert(len(points) == 1)
         return geojson.Point(points[0], validate=True)
+
+def tryToConvertToPolygon(tags, points):
+    tagsKeys = set(tags.keys())
+    # osm-multipolygon: means just as complex area ... but geojson polygons can also handle holes
+    if tagsKeys.intersection(POLYGON_TAGS) or tags.get("type") == "multipolygon":
+        if tags.get("type") == "multipolygon":
+            # due to coming already in right format
+            polygon = geojson.Polygon(points)
+        else:
+            # [points] as this is the outer linestring of the polygon (no holes in osm, if not specified) 
+            polygon = geojson.Polygon([points])
+        if not polygon.errors():
+            return polygon
+        else:
+            logging.debug("Could not be converted to a polygon with tags {}".format(tags))
+    line = geojson.LineString(points)
+    if not line.errors():
+        return line
+    else:
+        if tagsKeys.intersection(LINESTRING_TAGS):
+            logging.debug("Could not be converted to a linestring with tags {}".format(tags))
+        return geojson.MultiLineString(points, validate=True)
 
 def shapeGeomToGeoJson(shape, properties = None):
     geometry = mapping(shape)
