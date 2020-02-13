3952704ade5e235f71aa1b4e1fb81fb338e0171d
Florentin <florentin.doerre@tu-dresden.de>
2020-02-13 17:49:10 +0100

    Use STR tree index for building regions

--- urbanData/buildingComplexes.py
+++ urbanData/buildingComplexes.py
@@ -8,7 +8,7 @@ import networkx as nx
 from funcy import log_durations
 
 
-from shapely.geometry import Polygon, LineString, mapping, shape
+from shapely.geometry import Polygon, LineString, mapping, shape, MultiPolygon
 from shapely.ops import unary_union, transform
 from shapely.strtree import STRtree
 from OSMPythonTools.nominatim import Nominatim
@@ -35,7 +35,7 @@ from annotater.buildingLvlAnnotator import BuildingLvlAnnotator
 # TODO: function to map region id based for an object (based on address or just geometry)
 # tag order to analyse: public, leisure, amenity, buldings, landuse, office ? ... check if company?
 
-@log_durations(logging.info)
+@log_durations(logging.debug)
 def buildGroups(buildings):
     """groups buildings together, with at least one common point returns a geo-json featurecollections
         buildings: geojson featureCollection
@@ -61,6 +61,9 @@ def buildGroups(buildings):
     for id, indexes in enumerate(buildingComponents):
         buildingGeometries = [allBuildingsShapeGeom[index] for index in indexes]
         groupShape = unary_union(buildingGeometries)
+        # TODO: properly support MultiPolygons in geoJsonToFolium
+        if isinstance(groupShape, MultiPolygon):
+            groupShape = groupShape.convex_hull
         buildingIds = list(indexes)
 
         buildingGroup = shapeGeomToGeoJson(groupShape, properties={
@@ -76,64 +79,75 @@ def buildGroups(buildings):
 
     return geojson.FeatureCollection(buildingGroups)
 
-def buildRegions(buildingGroups, borders):
-    """buildingGroup expansion (if no borders inbetween -> union)"""
+@log_durations(logging.debug)
+def buildRegions(buildingGroups, borders, maxGroupDistance = 120):
+    """
+    buildingGroup expansion: if no borders inbetween and closer than maxGroupDistance -> union groups
+    """
 
-    buildingGroupGeoShapes = list(enumerate([withUTMCoord(shape(building["geometry"])) for building in buildingGroups["features"]]))
+    buildingGroupGeoShapes = [withUTMCoord(shape(building["geometry"])) for building in buildingGroups["features"]] 
 
     # borders between building-groups ! also need to be in UTM !
-    bordersShapelyLines = [withUTMCoord(shape(street["geometry"])) for street in borders["features"]] 
+    bordersShapelyLines = [withUTMCoord(shape(street["geometry"])) for street in borders["features"]]
+    for id, border in enumerate(bordersShapelyLines):
+        border.id = id
+    bordersIndex = STRtree(bordersShapelyLines)
 
     buildingGroupGraph = nx.Graph()
     # init graph 
-    for index, _ in buildingGroupGeoShapes:
-        buildingGroupGraph.add_node(index, borders = set())
-
+    for id, groupShape in enumerate(buildingGroupGeoShapes):
+        groupShape.id = id
+        buildingGroupGraph.add_node(id, borders = set())
+    
+    buildingGroupIndex = STRtree(buildingGroupGeoShapes)
     #visualize_edges = True
     #if visualize_edges:
     #    edges = folium.FeatureGroup("edges between home-groups")
 
     added_edges = 0
-    for index, bShape in buildingGroupGeoShapes:
-        if((index + 1) % 50 == 0 and not index == 0):
-            logging.info("Progress: {}/{} ; {} edges added".format(index + 1, len(buildingGroupGeoShapes), added_edges))
+    for bShape in buildingGroupGeoShapes:
+        index = bShape.id
+        if((index + 1) % 200 == 0 and not index == 0):
+            logging.info("Progress: {}/{} ; {} edges added".format(
+                index + 1, len(buildingGroupGeoShapes), added_edges))
             added_edges = 0
-    
-        for otherIndex, otherBShape in buildingGroupGeoShapes[index+1:]:
-            # TODO: Performance ? use shapely STRTree for querying this (need to set index attr in geometry for this! https://github.com/Toblerity/Shapely/issues/618)
-            # more than 120 meters inbetween -> very likely something in between
-            MAX_GROUP_DISTANCE = 120
-            distance = bShape.distance(otherBShape)
-            if distance < MAX_GROUP_DISTANCE:
-                center = bShape.centroid.coords[0]
-                otherCenter = otherBShape.centroid.coords[0]
-                connection = LineString(coordinates=[center, otherCenter])
 
-                crossesStreet = -1
-                for streetIndex, street in enumerate(bordersShapelyLines):
-                    if street.crosses(connection):
-                        crossesStreet = streetIndex
-                        break
+        nearbyBuildingGroups = buildingGroupIndex.query(bShape.buffer(maxGroupDistance))
+        for otherBShape in nearbyBuildingGroups:
+            otherIndex = otherBShape.id
+            center = bShape.centroid.coords[0]
+            # TODO: use not only center for group but center from each building
+            otherCenter = otherBShape.centroid.coords[0]
+            connection = LineString(coordinates=[center, otherCenter])
 
-                if crossesStreet == -1:
-                    added_edges += 1
-                    buildingGroupGraph.add_edge(index, otherIndex)
-                else:
-                    buildingGroupGraph.node[index]['borders'].add(crossesStreet)
-                    buildingGroupGraph.node[otherIndex]['borders'].add(crossesStreet)
+            crossesStreet = None
+            potentialBorders = bordersIndex.query(connection)
+            for street in potentialBorders:
+                if street.crosses(connection):
+                    crossesStreet = street.id
+                    break
+
+            if not crossesStreet:
+                added_edges += 1
+                buildingGroupGraph.add_edge(index, otherIndex)
+            else:
+                buildingGroupGraph.node[index]['borders'].add(
+                    crossesStreet)
+                buildingGroupGraph.node[otherIndex]['borders'].add(
+                    crossesStreet)
 
     regionComponents = nx.connected_components(buildingGroupGraph)
 
     buildingRegions = []
-    for id, indexes in enumerate(regionComponents):
-        groupsForRegion = [buildingGroups["features"][index] for index in indexes]
+    for id, groupIds in enumerate(regionComponents):
+        groupsForRegion = [buildingGroups["features"][index] for index in groupIds]
         groupForRegionGeometries = [shape(group["geometry"]) for group in groupsForRegion] 
         # TODO: use saved streets for this regions (for refining geometry) (find method for refinement)
         regionShape = unary_union(groupForRegionGeometries).convex_hull
 
-        borderIndexes = set.union(*[buildingGroupGraph.node[index]['borders'] for index in indexes])
+        borderIndexes = set.union(*[buildingGroupGraph.node[index]['borders'] for index in groupIds])
         regionBorders = [borders["features"][index]["properties"].get("name", "border{}".format(index)) for index in borderIndexes]
-        # remove duplicate street names (as streets often secmented by crossings)
+        # remove duplicate street names (as streets often segmented by crossings)
         regionBorders = list(set(regionBorders))
         groupIds = [group["properties"]["groupId"] for group in groupsForRegion]
 
@@ -220,13 +234,15 @@ if __name__ == "__main__":
     areaOfInterest = 'Pieschen, Dresden, Germany'
 
     pieschen = Nominatim().query(areaOfInterest)
-    allBuildingsQuery = OsmDataQuery("homes", OsmObjectType.WAY, ['"building"', 'abandoned!~"yes"'])
+    allBuildingsQuery = OsmDataQuery("homes", OsmObjectType.WAYANDRELATIONSHIP, ['"building"', 'abandoned!~"yes"'])
+    
     osmQueries = [ allBuildingsQuery,
             OsmDataQuery("borders", OsmObjectType.WAY,  
-            ['highway~"primary|primary_link|secondary|secondary_link|tertiary|tertiary_link|residential|motorway|unclassified"']),
+            ['highway~"primary|primary_link|secondary|secondary_link|tertiary|tertiary_link|residential|motorway|unclassified|living_street"']),
             OsmDataQuery("borders_railway", OsmObjectType.WAY, ["'railway'~'rail'"])]
 
     # https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#By_polygon_.28poly.29 for filtering based on polygon (if borough based on openDataDresden)
+    # this query can take a while
     osmData = OverPassHelper().directFetch(pieschen.areaId(), osmQueries)
 
     buildings = next(osmData)
@@ -237,7 +253,6 @@ if __name__ == "__main__":
 
         # Poor Mans Testing
         #buildings = geojson.FeatureCollection(buildings["features"][:200])
-
         logging.info("Fetched {} buildings".format(len(buildings["features"])))
         groups, regions = buildGroupsAndRegions(buildings, borders)
     else:
@@ -257,11 +272,16 @@ if __name__ == "__main__":
     # TODO: buildings with yes often lay inside f.i. hospital (amenity = hospital | healthcare = hospital) or landuse = police
     
     # TODO: clarify dependencies between them
-    annotater = [AddressAnnotator(areaOfInterest), BuildingLvlAnnotator(), CompanyAnnotator(),
-                 BuildingTypeClassifier(), OsmCompaniesAnnotator(
+    annotater = [AddressAnnotator(areaOfInterest), 
+                BuildingLvlAnnotator(), 
+                CompanyAnnotator(),
+                 OsmCompaniesAnnotator(
                      areaOfInterest, OsmObjectType.WAYANDNODE),
-                 LeisureAnnotator(areaOfInterest, OsmObjectType.WAYANDNODE), AmentiyAnnotator(areaOfInterest, OsmObjectType.WAYANDNODE), 
-                 SafetyAggregator(), EducationAggregator()]
+                BuildingTypeClassifier(),
+                LeisureAnnotator(areaOfInterest, OsmObjectType.WAYANDNODE), 
+                AmentiyAnnotator(areaOfInterest, OsmObjectType.WAYANDNODE), 
+                SafetyAggregator(), 
+                EducationAggregator()]
 
     for annotator in annotater:
         logging.info("Starting {}".format(annotator.__class__.__name__))
